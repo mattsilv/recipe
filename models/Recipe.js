dba1b752635b427c0802b45430668dac62746217
Matt Silverman <mattsilv@gmail.com>
2013-10-29 18:05:07 -0400

    working on recipe model

--- models/Recipe.js
+++ models/Recipe.js
@@ -13,13 +13,38 @@ var mongoose = require('mongoose');
 // reqired @boolean
 // default sets default if none is provided on creation
 
+// TODO JAMES: assign unique ID to each instance of an ingredient within a recipe
+
 // Start Schema Definition
+
+// Notes
+// serving_unit_qty - 5
+// serving_unit_name - Cup Chopped
+// measure_qty - 100
+// measure_uom - either ml or g, no other value accepted
 var recipeSchema = mongoose.Schema({
   name:{
     type:String,
     required:true
-  }
+  },
+  serving_unit_qty:{
+    type:Number,
+    required:true
+  },
+  serving_unit_name:{
+    type:String,
+    required:true
+  },
+  ingredients:[{
+    name:String,
+    serving_unit_qty:Number,
+    serving_unit_name:String,
+    measure_qty:Number,
+    measure_uom:String,
+    api_item_id:String
+  }]
 });
+
 
 
 // Created an index ensuring uniqueness on name
